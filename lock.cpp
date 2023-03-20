83613091f7eda7d8e9af1672b05aaa37e3d31fc1
Hobbit <thehobbitdyk@163.com>
2023-03-20 21:17:18 +0800

    modify mutex object to pointer in lock.*

--- lock.cpp
+++ lock.cpp
@@ -1,21 +1,25 @@
 #include "lock.h"
 
-Mutex::Mutex() {
-    if (pthread_mutex_init(&mutex, nullptr) != 0) {
+Mutex::Mutex() : mutex(new pthread_mutex_t()) {
+    if (pthread_mutex_init(mutex, nullptr) != 0) {
         throw std::exception();
     }
 }
 
 Mutex::~Mutex() {
-    if (pthread_mutex_destroy(&mutex) != 0) {
+    if (pthread_mutex_destroy(mutex) != 0) {
         throw std::exception();
     }
 }
 
+pthread_mutex_t* Mutex::get() {
+    return mutex;
+}
+
 int Mutex::lock() {
-    return pthread_mutex_lock(&mutex);
+    return pthread_mutex_lock(mutex);
 }
 
 int Mutex::unlock() {
-    return pthread_mutex_unlock(&mutex);
+    return pthread_mutex_unlock(mutex);
 }
